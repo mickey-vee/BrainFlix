8cf03570a679f07fd0141c7c140d050a06c2d24b
mickey-vee <mvoong27@gmail.com>
2024-07-09 16:40:20 -0400

    Styles added for video page

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+/* from array need to get title, channel, image for next videos  */
+
+/* "title": "Beyond the Horizon",
+"channel": "Emmett Wilson",
+"image": "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image4.jpg", */
---
+++ src/components/ComponentName/NextVideos.jsx
@@ -0,0 +1,13 @@
+/* const NextVideos = () => {
+  return `
+    
+    `;
+};
+
+<div className="video__card">
+  <div>
+    <h3 className="video__title"></h3>
+    <h3 className="video__author"></h3>
+  </div>
+</div>;
+ */
