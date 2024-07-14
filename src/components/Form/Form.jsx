0cdb42ad6c8df8d36798323de028bc5105f1e303
mickey-vee <mvoong27@gmail.com>
2024-07-14 14:48:51 -0400

    Tablet media added to page

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,17 +8,19 @@ const Form = () => {
         <div className="form__image"></div>
         <div className="form__form-box">
           <form id="form" className="form__wrapper">
-            <label className="form__label" htmlFor="form">
-              JOIN THE CONVERSATION
-            </label>
-            <textarea
-              className="form__text"
-              id="form"
-              name="user_comment"
-              rows="6"
-              cols="30"
-              placeholder="Add a new comment"
-            ></textarea>
+            <div className="form__input-wrapper">
+              <label className="form__label" htmlFor="form">
+                JOIN THE CONVERSATION
+              </label>
+              <textarea
+                className="form__text"
+                id="form"
+                name="user_comment"
+                rows="6"
+                cols="30"
+                placeholder="Add a new comment"
+              ></textarea>
+            </div>
 
             <div className="form__button">
               <button type="submit" className="form__submit" id="add-comment">
