606108e3f4526e125e2f5692a6f047b395b31d59
mickey-vee <mvoong27@gmail.com>
2024-07-14 14:06:31 -0400

    Stylings and media for video section

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import "./Video.scss";
 
 const Video = ({ video }) => {
+  const date = new Date(video.timestamp);
+
+  const formattedDate = date.toLocaleDateString("en-US", {
+    year: "2-digit",
+    month: "numeric",
+    day: "numeric",
+  });
+
   return (
     <>
       <section className="video" id="video">
@@ -9,16 +17,33 @@ const Video = ({ video }) => {
           className="video__image"
           src={video.video}
           poster={video.image}
+          controls
         ></video>
         <div className="video__wrapper">
           <h1 className="video__title">{video.title}</h1>
           <div className="video__detail">
-            <span className="video__detail-text video__detail-text--bold">
-              By {video.channel}
-            </span>
-            <span className="video__detail-text">{video.views} views</span>
-            <span className="video__detail-text">{video.timestamp}</span>
-            <span className="video__detail-text">{video.likes} likes</span>
+            <div className="video__detail-wrapper">
+              <span className="video__detail-text video__detail-text--bold">
+                By {video.channel}
+              </span>
+              <span className="video__detail-text">{formattedDate}</span>
+            </div>
+            <div className="video__detail-wrapper">
+              <span className="video__detail-text">
+                <img
+                  src="../../../src/assets/images/Icons/views.svg"
+                  alt="views icon"
+                />
+                {` ${video.views}`}
+              </span>
+              <span className="video__detail-text">
+                <img
+                  src="../../../src/assets/images/Icons/likes.svg"
+                  alt="likes icon"
+                />
+                {` ${video.likes}`}
+              </span>
+            </div>
           </div>
           <div>
             <p className="video__description">{video.description}</p>
